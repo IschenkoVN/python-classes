40edfb532137d29c588e09fdf2cf8570dbe6734a
Vladislav Ischenko <ischenkovn@gmail.com>
2014-04-30 14:08:30 +0300

    Added TemplateInit

--- final_exam.py
+++ final_exam.py
@@ -75,6 +75,9 @@ def print_table(data):
 input: str,
 operators: '+', '-', '*'
 
+Solutions:
+The Algorithm "Top-Down Parsing"
+
 """
 
 
@@ -179,12 +182,27 @@ print t.fill(x='12', y=13)      # prints "X=12 y=13"
 from string import Template
 
 
-s = Template('X=$x y=$y')
-print s.substitute(x='12', y='13')
+class TemplateInit(Template):
+    idpattern = '[a-z]+_[a-z]+'
+    delimiter = '{{'
+    pattern = r'''
+    \{\{(?:
+    (?P<escaped>\{\{)|
+    (?P<named>[_a-z][_a-z0-9]*)\}\}|
+    (?P<braced>[_a-z][_a-z0-9]*)\}\}|
+    (?P<invalid>)
+    )
+    '''
+
+    def __init__(self, filename):
+        with open(filename) as f:
+            lines = [i.strip() for i in f]
+        super(TemplateInit, self).__init__('\n'.join(lines))
 
 
 if __name__ == '__main__':
 
+    # Print Table
     data = {
         "O": ['12345', '12346', '1234567', '12'],
         "h0": ['val1', 'val12', 'val123', 'val1234'],
@@ -192,11 +210,19 @@ if __name__ == '__main__':
         "h3": ['12345', '12346', '1234567', '12345678'],
     }
     print print_table(data)
-
     # Just for fun =) One row.
     res = '\n'.join(['+' + '+'.join(['-' * (len(max(max([max(elem_list, key=len) for elem_list in data.itervalues()], key=len), max(data.keys(), key=len), key=len)) + 2) for key in data.keys()]) + '+'] + ['| ' + ' | '.join([key.center(len(max(max([max(elem_list, key=len) for elem_list in data.itervalues()], key=len), max(data.keys(), key=len), key=len))) for key in data.keys()]) + ' |'] + ['+' + '+'.join(['-' * (len(max(max([max(elem_list, key=len) for elem_list in data.itervalues()], key=len), max(data.keys(), key=len), key=len)) + 2) for key in data.keys()]) + '+'] +['| '+' | '.join([str(elem).center(len(max(max([max(elem_list, key=len) for elem_list in data.itervalues()], key=len), max(data.keys(), key=len), key=len))) for elem in value_couples])+' |' for value_couples in zip(*data.values())] + ['+' + '+'.join(['-' * (len(max(max([max(elem_list, key=len) for elem_list in data.itervalues()], key=len), max(data.keys(), key=len), key=len)) + 2) for key in data.keys()]) + '+'])
     assert print_table(data) == res
 
+    # Arithmetic expression "Top-Down Parsing"
     assert parse("1 +    2 +  7") == 1 +    2 +  7
     assert parse("1 + 1.5 * 4 + 3") == 1 + 1.5 * 4 + 3
     assert parse("1 + 1.5+1.5* 96") == 1 + 1.5+1.5* 96
+
+    # Rendering variables to templates
+    filename = 'templates.txt'
+    with open(filename, "w") as f:
+        f.write('X={{x}} y={{y}}')
+
+    ti = TemplateInit(filename)
+    assert ti.safe_substitute(x='12', y=13) == "X=12 y=13"
